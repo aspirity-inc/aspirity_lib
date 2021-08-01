e80db8c63135c38b36392ec7ca961d65b473eaa8
PavelZubkov <zubkov_p@mail.ru>
2021-08-01 21:46:53 +0300

    Init

---
+++ plural/plural.ts
@@ -0,0 +1,39 @@
+namespace $ {
+
+	export class $aspirity_plural extends $mol_object2 {
+
+		other() {
+			throw new Error(`${ this }.other() is not defined`)
+		}
+		
+		one() {
+			throw new Error(`${ this }.one() is not defined`)
+		}
+		
+		two() {
+			throw new Error(`${ this }.two() is not defined`)
+		}
+		
+		few() {
+			throw new Error(`${ this }.few() is not defined`)
+		}
+		
+		many() {
+			throw new Error(`${ this }.many() is not defined`)
+		}
+		
+		@ $mol_mem
+		plural_rules() {
+			const locale = this.$.$mol_locale.lang()
+			return new Intl.PluralRules(locale)
+		}
+		
+		@ $mol_mem
+		select( num : number ) {
+			const plural = this.plural_rules().select( num )
+			return this[ plural ]()
+		}
+
+	}
+
+}
